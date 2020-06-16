8fa3f13486eb0544466398df751b42e18d23fc0b
jedling <joel.edling1@gmail.com>
2020-06-16 22:25:23 +0200

    responsive map stylin

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -3,12 +3,6 @@ import withPageWrapper from '../../hocs/withPageWrapper';
 import './style.css';
 
 const Contact = () => {
-  const mapStyle = {
-    width: '100%',
-    height: '360px',
-    border: '15px solid white',
-    margin: 'margin-top: auto'
-  };
 
   return (
     <section className="contact">
@@ -54,9 +48,9 @@ const Contact = () => {
       <div className="row align-bottom-container">
         <div className="column map">
           <iframe
+            className="map-style"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2254.1898708828157!2d12.995645816004979!3d55.5987091113333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653a156c18d5f81%3A0x98f24303706148b3!2sErik%20Dahlbergsgatan%203%2C%20211%2048%20Malm%C3%B6!5e0!3m2!1ssv!2sse!4v1592312168405!5m2!1ssv!2sse"
             frameBorder="0"
-            style={mapStyle}
             allowFullScreen=""
             aria-hidden="false"
             tabIndex="0"
